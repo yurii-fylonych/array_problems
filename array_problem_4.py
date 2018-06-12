ffa84ab93873bd96b7a5759d87b137d8902d4c42
yurii-fylonych <yuraenergy92@gmail.com>
2018-06-12 13:50:19 +0300

    Task 4. Using while loop

--- array_problem_4.py
+++ array_problem_4.py
@@ -12,3 +12,26 @@ def for_sum(array):
 
     return sum
 
+
+
+
+def while_sum(array):
+    sum = 0
+    index = 0
+
+    if len(array) == 0:
+        return None, 'Please, Try Again. Your array is too short'
+
+    while len(array) > index:
+        sum += array[index]
+        index += 2
+
+    return sum, None
+
+sum, error = while_sum(array)
+
+if error != None:
+    print(error)
+if error is None:
+    print(sum)
+
