9f3672cc1415d73b6131c72b1b7040ed1090fcb3
yurii-fylonych <yuraenergy92@gmail.com>
2018-06-13 11:37:24 +0300

    Task 8. Using for loop

---
+++ array_problem_8.py
@@ -0,0 +1,23 @@
+array = [10, 3, 16, 99, 22, 100, 44, 22, -12, 33, 21]
+
+
+def bubble_sort(array):
+    if len(array) == 0 or len(array) == 1:
+        return None, 'Short array. Try again'
+    length = len(array) - 1
+    for i in range(length):
+        for j in range(length - i):
+            if array[j] > array[j + 1]:
+                array[j], array[j + 1] = array[j + 1], array[j]
+                print(array)
+    return array, None
+
+
+result, error = bubble_sort(array)
+
+if error != None:
+    print(error)
+else:
+    print(result)
+
+
