39aa97382ebc7d8631683ea30361aca8b9da6914
yurii-fylonych <yuraenergy92@gmail.com>
2018-06-13 11:37:58 +0300

    Task 8. Using while loop

--- array_problem_8.py
+++ array_problem_8.py
@@ -21,3 +21,27 @@ else:
     print(result)
 
 
+def bubble_sort(array):
+    if len(array) == 0 or len(array) == 1:
+        return None, 'Short array. Try again'
+
+    j = 0
+
+    length = len(array) - 1
+
+    while j < length:
+        i = 0
+        while i < length:
+            if array[i] > array[i + 1]:
+                array[i], array[i + 1] = array[i + 1], array[i]
+            i += 1
+        j += 1
+    return array, None
+
+
+result, error = bubble_sort(array)
+
+if error != None:
+    print(error)
+else:
+    print(result)
