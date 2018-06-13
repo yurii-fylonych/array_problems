35bf9736df1e7f8b6800151304a39b35c71dfbf4
yurii-fylonych <yuraenergy92@gmail.com>
2018-06-13 11:35:38 +0300

    Task 7. Using while loop

--- array_problem_7.py
+++ array_problem_7.py
@@ -19,3 +19,25 @@ if error == None:
     print(list_of_array)
 
 
+def while_odd_item(array):
+    if len(array) == 0:
+        return None, 'Short array. Try again'
+
+    index = 1
+
+    while index < len(array):
+        print(array[index])
+        index += 1
+
+    return array, None
+
+list_of_array, error = while_even_item(array)
+
+if error != None:
+    print(error)
+if error == None:
+    print(list_of_array)
+
+
+
+
