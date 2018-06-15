e49662bf8ce2a7138bd4d82588d122e66db90dc3
yurii-fylonych <yuraenergy92@gmail.com>
2018-06-15 11:02:42 +0300

    Task 11. While loop

--- array_problem_11.py
+++ array_problem_11.py
@@ -21,3 +21,28 @@ if error != None:
 if error == None:
     print(result)
 
+
+def get_mim_item(array):
+    if len(array) == 0:
+        return None, 'Short array. Try again'
+
+    min_el = array[0]
+    index = 0
+
+    while index < len(array):
+        if index % 2 != 0:
+            if array[index] < min_el:
+                min_el = array[index]
+        index += 1
+
+    return min_el, None
+
+
+result, error = get_mim_item(array)
+
+if error != None:
+    print(error)
+if error == None:
+    print(result)
+
+
