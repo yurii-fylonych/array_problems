80d0a5bfb5acc9b61d725cc595e9491d4146d761
yurii-fylonych <yuraenergy92@gmail.com>
2018-06-15 11:00:41 +0300

    Task 10.For loop

---
+++ array_problem_10.py
@@ -0,0 +1,22 @@
+array = [10, 3, 16, 99, 22, 100, 44, 22, -12, 33, 21]
+
+
+def array_with_even_items(array):
+    if len(array) == 0:
+        return None, 'Short array. Try again'
+
+    B = []
+
+    for index, item in enumerate(array):
+        if index % 2 != 0:
+            B.append(item)
+
+    return B, None
+
+
+result, error = array_with_even_items(array)
+
+if error != None:
+    print(error)
+else:
+    print(result)
