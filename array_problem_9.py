7c491f9ff9c549c3288896cb1884a8c18462e526
yurii-fylonych <yuraenergy92@gmail.com>
2018-06-15 10:59:55 +0300

    Task 9. Recursion

--- array_problem_9.py
+++ array_problem_9.py
@@ -43,4 +43,32 @@ if error != None:
 else:
     print(result)
 
+def array_with_even_items(array):
+    if len(array) == 0:
+        return None, 'Short array. Try again'
 
+    B = []
+
+    def rec_array(array, B):
+        if len(array) == 0:
+            return B
+        if array[0] % 2 == 0:
+            B.append(array[0])
+        return rec_array(array[1:], B)
+
+    result = rec_array(array, B)
+
+    return result, None
+
+
+result, error = array_with_even_items(array)
+
+if error != None:
+    print(error)
+else:
+    print(result)
+
+
+get_array = list(filter(lambda x: x % 2 == 0, array))
+
+B = [item for item in array if item % 2 == 0]
