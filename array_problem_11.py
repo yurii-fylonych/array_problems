08545bc621fafdf4b4686f906c9cccc8f517d45a
yurii-fylonych <yuraenergy92@gmail.com>
2018-06-15 11:03:02 +0300

    Task 11. Recursion

--- array_problem_11.py
+++ array_problem_11.py
@@ -46,3 +46,30 @@ if error == None:
     print(result)
 
 
+
+def get_mim_item(array):
+    if len(array) == 0:
+        return None, 'Short array. Try again'
+
+    min_el = array[0]
+
+    def get_min_rec(array, min_el):
+        if len(array) <= 1:
+            return min_el
+        if array[1] < min_el:
+            min_el = array[1]
+            return get_min_rec(array[2:], min_el)
+        else:
+            return get_min_rec(array[2:], min_el)
+
+    result = get_min_rec(array, min_el)
+
+    return result, None
+
+
+result, error = get_mim_item(array)
+
+if error != None:
+    print(error)
+if error == None:
+    print(result)
