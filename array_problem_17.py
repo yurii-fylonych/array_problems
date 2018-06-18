8b130914a8579c7e6d1061743fac121d348572e1
yurii-fylonych <yuraenergy92@gmail.com>
2018-06-18 17:19:05 +0300

    Task 17. Recursion

--- array_problem_17.py
+++ array_problem_17.py
@@ -47,3 +47,32 @@ if result == None:
 else:
     print('Array has {} negative elements'.format(result))
 
+
+def count_negative_items(array):
+    if len(array) == 0:
+        return None, 'Short array. Try again'
+
+    count = 0
+    index = 0
+
+    def rec_has(array, count):
+        if len(array) == 0:
+            return count
+
+        if array[0] < 0:
+            count += 1
+
+        return rec_has(array[1:], count)
+
+    count = rec_has(array, count)
+
+    return count, None
+
+
+result, error = count_negative_items(array)
+
+if result == None:
+    print(error)
+else:
+    print('Array has {} negative elements'.format(result))
+
