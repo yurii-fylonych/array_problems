765b1e69161820abbafdf05257f173b008e181b2
yurii-fylonych <yuraenergy92@gmail.com>
2018-06-18 17:15:01 +0300

    Task 16. For loop

---
+++ array_problem_16.py
@@ -0,0 +1,27 @@
+array = [10, 3, 16, 99, 22, 100, 44, 22, -12, 33, 21]
+
+
+def find_2_max_item(array):
+    if len(array) == 0:
+        return None, 'Short array. Try again'
+
+    max_el = array[0]
+    sec_el = array[1]
+    for item in array:
+        if item > max_el:
+            max_el = item
+
+    for item in array:
+        if item > sec_el and item != max_el:
+            sec_el = item
+
+    return sec_el, None
+
+
+result, error = find_2_max_item(array)
+
+if result == None:
+    print(error)
+else:
+    print(result)
+
