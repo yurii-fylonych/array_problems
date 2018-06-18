40fcca1e903c7db763da37cb855043738763a807
yurii-fylonych <yuraenergy92@gmail.com>
2018-06-18 17:18:24 +0300

    Task 17. While loop

--- array_problem_17.py
+++ array_problem_17.py
@@ -21,3 +21,29 @@ if result == None:
     print(error)
 else:
     print('Array has {} negative elements'.format(result))
+
+
+
+def has_negative_items(array):
+    if len(array) == 0:
+        return None, 'Short array. Try again'
+
+    count = 0
+    index = 0
+
+    while index < len(array):
+        if array[index] < 0:
+            count += 1
+
+        index += 1
+
+    return count, None
+
+
+result, error = has_negative_items(array)
+
+if result == None:
+    print(error)
+else:
+    print('Array has {} negative elements'.format(result))
+
