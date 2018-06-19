2a11d2be405137fb14228d46c034b1dda6c941fc
yurii-fylonych <yuraenergy92@gmail.com>
2018-06-19 11:10:47 +0300

    Task 20. While loop.

--- array_problem_20.py
+++ array_problem_20.py
@@ -21,3 +21,28 @@ if result == None:
     print(error)
 else:
     print(result)
+
+
+
+def substitute_items(array):
+    if len(array) == 0:
+        return None, 'Short array. Try again'
+
+    count = 0
+    index = 0
+
+    while index < len(array):
+        if array[index] > 7:
+            array[index] = 7
+            count += 1
+        index += 1
+
+    return count, None
+
+
+result, error = substitute_items(array)
+
+if result == None:
+    print(error)
+else:
+    print(result)
