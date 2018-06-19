dd1b046181c2ceb29b4d4db4cda3fba7ce51e05a
yurii-fylonych <yuraenergy92@gmail.com>
2018-06-19 11:07:47 +0300

    Task 19. For loop.

---
+++ array_problem_19.py
@@ -0,0 +1,23 @@
+array = [10, 3, 16, 99, 22, 100, 44, 22, -12, 33, 21]
+
+
+def substitute_items(array):
+    if len(array) == 0:
+        return None, 'Short array. Try again'
+
+    for index, item in enumerate(array):
+        if item > 0:
+            array[index] = 1
+        else:
+            array[index] = 0
+
+    return array, None
+
+
+result, error = substitute_items(array)
+
+if result == None:
+    print(error)
+else:
+    print(result)
+
