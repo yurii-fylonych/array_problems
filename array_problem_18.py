06b788c51c26802d0ac9c30f2899847b6a567a88
yurii-fylonych <yuraenergy92@gmail.com>
2018-06-19 11:06:07 +0300

    Task 18. For loop.

---
+++ array_problem_18.py
@@ -0,0 +1,23 @@
+array = [10, 3, 16, 99, 22, 100, 44, 22, -12, 33, 21]
+
+
+def min_items(array):
+    if len(array) <= 2:
+        return None, 'Short array. Try again'
+
+    min_item = 10000
+
+    for index, item in enumerate(array):
+        if index % 2 == 0:
+            if min_item > item > 0:
+                min_item = item
+
+    return min_item, None
+
+
+result, error = min_items(array)
+
+if result == None:
+    print(error)
+else:
+    print(result)
